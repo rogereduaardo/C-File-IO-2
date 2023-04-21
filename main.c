2d4d354a52fed33e8de5da777448b48499f9eebf
“Rogereduaardo” <remmtools98@gmail.com>
2023-04-21 16:30:26 +0000

    Error on path added hello.txt spelled worng intellionally

--- main.c
+++ main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
 int main(void) {
-printf("C File I/0 - System Calls\n");
+printf("C File I/O - System Calls\n");
+  
+  // fd = file descriptor 
+  // int open (const char *pathname, int flags);
+  const char *pathname = "hello.txtas";
+  int flags = O_RDONLY;
+  int fd = open (pathname, flags);
+  printf("fd: %d\n", fd);
+  if (-1 == fd)
+  {
+    perror("open");
+    return 1;
+  }
+  
 return 0;
+  
 }
