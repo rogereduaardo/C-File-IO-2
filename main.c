193186ac76b7670e2503fc1e89a0f428d5277114
“Rogereduaardo” <remmtools98@gmail.com>
2023-04-21 16:55:59 +0000

    Fixed bugg

--- main.c
+++ main.c
@@ -3,13 +3,14 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <unistd.h>
 
 int main(void) {
 printf("C File I/O - System Calls\n");
   
   // fd = file descriptor 
   // int open (const char *pathname, int flags);
-  const char *pathname = "hello.txtas";
+  const char *pathname = "hello.txt";
   int flags = O_RDONLY;
   int fd = open (pathname, flags);
   printf("fd: %d\n", fd);
@@ -18,6 +19,16 @@ printf("C File I/O - System Calls\n");
     perror("open");
     return 1;
   }
+
+  int result = 1;
+  while(result != 0)
+    {
+      char ch;
+      result = read(fd, &ch, 1)git ;
+      printf("result: %d, ch: 0x%02x %c\n", result, ch, ch);
+    }
+
+  close(fd);
   
 return 0;
   
